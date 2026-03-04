c7e6ed9f1e699e13f420f872c680eeeb86256d1f
NguyenKimTien79 <102939444+NguyenKimTien-07092003@users.noreply.github.com>
2026-03-04 16:45:52 +0700

    Update solution for bag-of-words

--- bag-of-words/bag-of-words.py
+++ bag-of-words/bag-of-words.py
@@ -2,14 +2,11 @@ import numpy as np
 
 def bag_of_words_vector(tokens, vocab):
     vector = np.zeros(len(vocab), dtype=int)
-    
-    # Duyệt từng từ trong vocab
+
     for i, word in enumerate(vocab):
         count = 0
-        # Duyệt toàn bộ tokens để đếm
         for token in tokens:
             if token == word:
                 count += 1
         vector[i] = count
-    
     return vector
